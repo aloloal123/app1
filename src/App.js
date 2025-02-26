5385ddc592285fe9f8aed656379241641b98a970
aloloal123 <aloloal409@gmail.com>
2025-02-26 14:34:41 +0700

    add Bootstrap and form-text.js
    add Bootstrap and form-text.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import React, { useState } from 'react'; 
-import { userContext } from './context'; 
-import Header2 from './context-header2'; 
-import Content2 from './context-content2'; 
+import FormText from './form-text';
+ 
 
 export default function App() {
-  const [user, setUser] = useState(''); 
-
-  return (
-    <userContext.Provider value={[user, setUser]}> 
-      <Header2 /> 
-      <Content2 />
-    </userContext.Provider>
-  );
+  return <FormText/>
 }
